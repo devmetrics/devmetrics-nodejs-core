fd8ef0199aa2cc731700c284d615d28076079f69
andrewandrew <andrew@zakonov.com>
2015-07-14 02:21:11 +0300

    better metric naming

--- test/test_devmetrics.js
+++ test/test_devmetrics.js
@@ -1,5 +1,5 @@
-var devmetrics = require('./../lib/devmetrics-core')();
+var devmetrics = require('./../lib/devmetrics-core')({'host': 'it.devmetrics.io', 'app_id': 'lib_test'});
 devmetrics.info('hey');
 devmetrics.error('error log');
-devmetrics.userEvent('hey_event', 123, ['osx']);
+devmetrics.userEvent('hey_event', 123, ['osx', 'andrew']);
 devmetrics.appEvent('app_started', ['osx']);
